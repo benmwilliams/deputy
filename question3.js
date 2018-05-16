290235b67c9552e8f14f7bd5d1fcaffcaf35eb30
Ben Williams <benwilliamsdev@gmail.com>
2018-05-15 20:43:12 -0400

    corrected questions 3, 4

--- question3.js
+++ question3.js
@@ -2,12 +2,14 @@ function validateRoster(obj1, obj2) {
     let start1 = obj1.StartTime,
         end1 = obj1.EndTime,
         start2 = obj.StartTime,
-        end2 = obj2.EndTime
-    valid = true;
+        end2 = obj2.EndTime,
+        rosterIsValid = false;
 
-    if (end2 < start1 || start2 < end1) {
-        valid = false;
+    if (start1 < end1 && start2 < end2) {
+        if (start2 > end1 || end2 < start1) {
+            rosterIsValid = true;
+        }
     }
 
-    return valid
+    return rosterIsValid
 }
--- question4.js
+++ question4.js
@@ -1,3 +1,7 @@
+// "Naturally this cascading parent-child relation means that
+// Location Manager, Supervisor, Employee are all subordinate
+// roles to System Administrator".... ah- *all* sub roles, not just immediate
+
 function findSubs(id) {
     let user = usersArray.find(obj => {
         return obj.Id === id;
@@ -7,7 +11,11 @@ function findSubs(id) {
         return role.Parent === userRole;
     });
     let subs = usersArray.filter(users => {
-        return users.Role === subRole.Id;
+        try {
+            return users.Role >= subRole.Id;
+        } catch (err) {
+            console.log('error: this user has no suboordinates');
+        }
     });
 
     return subs
